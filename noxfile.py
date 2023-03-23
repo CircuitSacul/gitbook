ed80a901d9e67b1c61c9f18070dc62732c6fb064
CircuitSacul <circuitsacul@icloud.com>
2023-03-22 20:42:47 -0400

    update CI & linting

--- noxfile.py
+++ noxfile.py
@@ -2,26 +2,19 @@ import nox
 
 
 @nox.session
-def mypy(session: nox.Session) -> None:
+def check(session: nox.Session) -> None:
     session.install("poetry")
     session.run("poetry", "install")
 
-    session.run("mypy", ".")
-
-
-@nox.session(name="apply-lint")
-def apply_lint(session: nox.Session) -> None:
-    session.install("black")
-    session.install("isort")
-    session.run("black", ".")
-    session.run("isort", ".")
+    session.run("mypy", ".", external=True)
+    session.run("ruff", ".", external=True)
+    session.run("black", ".", "--check", external=True)
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("black")
-    session.install("flake8")
-    session.install("isort")
-    session.run("black", ".", "--check")
-    session.run("flake8")
-    session.run("isort", ".", "--check")
+    session.install("poetry")
+    session.run("poetry", "install")
+
+    session.run("ruff", ".", "--fix", external=True)
+    session.run("black", ".", external=True)
